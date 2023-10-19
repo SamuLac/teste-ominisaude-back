263eca15287a44712f4c691bfdbe9e1872d5804d
Samuel Lacerda <samuel.lacerdaif@gmail.com>
2023-10-19 11:07:31 -0300

    feat: criando interface gráfica

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from tkinter import *
 from classes.movie import Movie
 
 
@@ -12,5 +13,41 @@ def request_data(name):
 
 
 response = request_data("avatar")
-movie = Movie(response['Title'], response['Year'], response['Rated'], response['Plot'], response['Director'])
-print(movie.getTitle(), movie.getYear(), movie.getRating(), movie.getPlot(), movie.getDirector())
+movie = Movie(
+    response["Title"],
+    response["Year"],
+    response["Rated"],
+    response["Plot"],
+    response["Director"],
+)
+# print(
+#   movie.getTitle(),
+#   movie.getYear(),
+#    movie.getRating(),
+#    movie.getPlot(),
+#    movie.getDirector(),
+#)
+
+if __name__ == '__main__':
+  window = Tk()
+  window.title("OmniMovies")
+  window.config(padx=100, pady=100)
+  window.geometry("600x400")
+
+  search_label = Label(text="Nome do Filme:")
+  search_label.grid(row=2, column=0)
+
+  movie_entry = Entry(width=30)
+  movie_entry.grid(row=2, column=1, columnspan=2)
+  movie_entry.focus() 
+
+  search_button = Button(text="Pesquisar", width=20)
+  search_button.grid(row=4, column= 1, pady= 10)
+
+  favorite_label = Label(text="Filmes Favoritos:")
+  favorite_label.grid(row=80, column=0, pady= 20)
+  
+  favorite_button = Button(text="Listar", width=20)
+  favorite_button.grid(row=80, column=1, pady= 20)
+  
+  window.mainloop()
